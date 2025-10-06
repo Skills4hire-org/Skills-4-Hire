f2b148f1e42815fd22d9fbb7556be37f09258b6b
Gideon Onita <onitagideon30@gmail.com>
2025-10-06 17:30:09 +0100

    fix:rectify max-width for bookings tab head

--- src/pages/CustomerBookings.tsx
+++ src/pages/CustomerBookings.tsx
@@ -7,19 +7,12 @@ import BookingsTabContent from '@/components/bookings/BookingsTabContent'
 
 export default function CustomerBookings() {
   return (
-    <div className="pt-4 pb-8 space-y-6">
-      <Container>
-        <MobileBookingsHeader />
-      </Container>
-      <Tabs
-        defaultValue="ongoing"
-        className="w-full max-w-6xl xl:max-w-7xl mx-auto"
-      >
+    <Container className="pt-4 pb-8">
+      <MobileBookingsHeader />
+      <Tabs defaultValue="ongoing" className="w-full mt-6">
         <TabHead tabList={bookingsTabsList} />
-        <Container>
-          <BookingsTabContent />
-        </Container>
+        <BookingsTabContent />
       </Tabs>
-    </div>
+    </Container>
   )
 }
