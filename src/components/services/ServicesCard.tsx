3f1281289188a464e7f6d586f7e74079d372bade
Gideon Onita <onitagideon30@gmail.com>
2025-10-13 09:23:47 +0100

    fix:adjust mobile sidebar width and refactor services card

--- src/components/services/ServicesCard.tsx
+++ src/components/services/ServicesCard.tsx
@@ -1,13 +1,9 @@
-import AddToFavoriteForm from '../form/AddToFavoriteForm'
-
 export default function ServicesCard({
   serviceName,
   serviceImage,
-  favorite,
 }: {
   serviceName: string
   serviceImage: string
-  favorite: boolean
 }) {
   return (
     <figure className="relative rounded-xl">
@@ -21,11 +17,6 @@ export default function ServicesCard({
         <span className="text-white capitalize text-[10px] sm:text-xs absolute bottom-4 font-medium left-1/2 -translate-x-1/2 py-1 px-2 bg-black/30 w-full text-center">
           {serviceName}
         </span>
-        <AddToFavoriteForm
-          id={serviceName}
-          type="service"
-          favorite={favorite}
-        />
       </figcaption>
     </figure>
   )
