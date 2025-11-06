914ca007fa8df668781e4228472111af63d2ea0b
Iyanuoluwatomiwa <odewenuolamide@gmail.com>
2025-11-06 12:54:49 +0000

    changed brand-logo

--- src/components/global/Logo.tsx
+++ src/components/global/Logo.tsx
@@ -1,9 +1,13 @@
-import logo from '@/assets/images/logo.png'
+import logo from "@/assets/images/logo.png";
 
 export default function Logo() {
   return (
-    <figure>
-      <img src={logo} alt="logo" className="w-20" width={108} height={62} />
+    <figure className="relative overflow-hidden flex justify-center items-center w-20 md:w-24 h-auto">
+      <img
+        src={logo}
+        alt="logo"
+        className="scale-[1.3] md:scale-[1.5] object-contain object-center"
+      />
     </figure>
-  )
+  );
 }
--- src/components/global/Logo2.tsx
+++ src/components/global/Logo2.tsx
@@ -1,15 +1,13 @@
-import logo from '@/assets/images/logo2.png'
+import logo from "@/assets/images/logo2.png";
 
-export default function Logo2() {
+export default function Logo() {
   return (
-    <figure>
+    <figure className="relative overflow-hidden flex justify-center items-center w-16 md:w-20 lg:w-24 h-auto">
       <img
         src={logo}
         alt="logo"
-        className="w-20 md:w-32"
-        width={133}
-        height={100}
+        className="scale-[1.3] md:scale-[1.5] object-contain object-center"
       />
     </figure>
-  )
+  );
 }
