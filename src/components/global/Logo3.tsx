b7ab41645cc50734a5e78d0c4e1394b183393460
Gideon Onita <onitagideon30@gmail.com>
2025-12-26 10:32:28 +0100

    feat: add another logo type

---
+++ src/components/global/Logo3.tsx
@@ -0,0 +1,9 @@
+import logo from '@/assets/images/logo3.png'
+
+export default function Logo3() {
+  return (
+    <figure>
+      <img src={logo} alt="logo" width={38} height={35} />
+    </figure>
+  )
+}
