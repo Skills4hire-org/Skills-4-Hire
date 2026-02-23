0fbeb1593aa2dfbd322a603fb787b333edfe7ba4
Gideon Onita <onitagideon30@gmail.com>
2026-02-23 09:32:23 +0100

    modify padding for button

--- src/components/buttons/SwitchRoleButton.tsx
+++ src/components/buttons/SwitchRoleButton.tsx
@@ -58,7 +58,7 @@ export default function SwitchRoleButton({ className }: { className: string }) {
       ) : (
         <Link to="/service-provider/registration">
           <button
-            className={`flex items-center justify-start px-4 py-2 rounded-full w-max text-xs font-medium gap-2.5 ${className} cursor-pointer`}
+            className={`flex items-center justify-start px-4 py-2 rounded-full w-max text-xs font-medium gap-1.5 ${className} cursor-pointer`}
           >
             <MdHandyman className="w-5 h-5 font-bold" />
             Register as a Service Provider
